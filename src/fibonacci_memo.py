0f22cc476af6923f7b8713d64ec1ef501ea8808f
Celine Chahine <chahineceline@gmail.com>
2025-01-07 22:32:21 -0500

    Implemented Memo

---
+++ src/fibonacci_memo.py
@@ -0,0 +1,6 @@
+def fibonacci_memo(n, memo={}) :
+    if n <=1:
+        return n
+    if n not in memo :
+        memo[n]= fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
+    return memo[n]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import time
 from fibonacci_naive import fibonacci_naive
+from fibonacci_memo import fibonacci_memo
 
 def main():
     n = 2
@@ -7,6 +8,9 @@ def main():
     print(f"Naive Recursive Fibonacci({n}): {fibonacci_naive(n)}")
     print(f"Naive Recursive Time: {time.time() - start:.5f} seconds")
 
+    start = time.time()
+    print(f"Memo Recursive Fibonacci({n}): {fibonacci_memo(n)}")
+    print(f"Memo Recursive Time: {time.time() - start:.5f} seconds")
 
 if __name__ == "__main__":
     main()
